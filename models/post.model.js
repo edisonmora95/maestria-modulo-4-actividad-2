6dd69970beb590884baabe259d7279432446de4a
Edison Mora <edison_andre_9@hotmail.com>
2021-10-31 11:14:09 -0500

    Added post model

--- models/post.model.js
+++ models/post.model.js
@@ -1,46 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
   text: {
     type: String,
-    required: 'text is required',
-    maxlength: 300
+    required: true,
+    maxlength: 300,
+    minlength: 5,
+  },
+  title: {
+    type: String,
+    required: true,
+    maxlength: 300,
+    minlength: 5,
   },
   author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    type: String,
+    required: true,
   },
-  image: {
-    type: String
-  }
 }, { 
   timestamps: true,
-  toJSON: {
-    virtuals: true,
-    transform: function (doc, ret) {
-      ret.id = ret._id;
-      delete ret.__v;
-      delete ret._id;
-      return ret;
-    }
-  }
 });
 
-schema.virtual('comments', {
-  ref: 'Comment',
-  localField: '_id',
-  foreignField: 'post',
-  justOne: false,
-});
+const Post = mongoose.model("Post", schema);
 
-schema.virtual('likes', {
-  ref: 'Like',
-  localField: '_id',
-  foreignField: 'post',
-  count: true,
-});
-
-const Post = mongoose.model('Post', schema);
 module.exports = Post;
