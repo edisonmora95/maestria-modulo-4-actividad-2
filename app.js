f28c3fd00a06a536e26f262513fe40b03174ed5f
Edison Mora <edison_andre_9@hotmail.com>
2021-10-31 11:18:24 -0500

    Instaled dependencies

--- app.js
+++ app.js
@@ -1,43 +1,33 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const createError = require('http-errors');
-const express = require('express');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const passport = require('passport');
-
-require('./config/passport.config');
-require('./config/db.config');
-const cors = require('./config/cors.config');
-const session = require('./config/session.config');
+const createError = require("http-errors");
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const app = express();
 
 /** Middlewares */
-app.use(logger('dev'));
-app.use(session);
-app.use(cors);
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(logger("dev"));
 app.use(express.json());
 
 /** Routes */
-const routes = require('./config/routes.config');
-app.use('/api', routes);
+const routes = require("./config/routes.config");
+app.use("/api", routes);
 
 /** Error Handling */
 
-app.use((req, res, next) => {
-  next(createError(404, 'Route not found'))
+app.use((_req, _res, next) => {
+  next(createError(404, "Route not found"));
 })
 
 app.use((error, req, res, next) => {
   if (error instanceof mongoose.Error.ValidationError) {
     error = createError(400, error);
-  } else if (error instanceof mongoose.Error.CastError && error.message.includes('_id')) {
-    error = createError(404, 'Resource not found');
-  } else if (error.message.includes('E11000')) {
-    error = createError(409, 'Duplicated');
+  } else if (error instanceof mongoose.Error.CastError && error.message.includes("_id")) {
+    error = createError(404, "Resource not found");
+  } else if (error.message.includes("E11000")) {
+    error = createError(409, "Duplicated");
   } else if (!error.status) {
     error = createError(500, error);
   }
